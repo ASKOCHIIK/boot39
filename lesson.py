06bf28ad83548f8c19e6c64fa1cc91911bd20c64
Askat Kulmanov <askatkulmanov1gmail.com@MacBook-Air-Askat.local>
2026-01-28 12:07:42 +0600

    исправлены ошибки в коде

--- lesson.py
+++ lesson.py
@@ -1,6 +1,7 @@
 class Mentor:
-    def __init__(self, name)
+    def __init__(self, name):
         self.name = name
-	self.junior = False
-    def info(self)
-	return 'hello', self.name
+        self.junior = False
+
+    def info(self):
+        return f'{self.name} junior' if self.junior else f'{self.name}'
