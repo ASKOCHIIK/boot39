b75b07a5b42d16b4bfb3d112a9af0210ddd7dc95
bakbergen3101 <kubatbekovbakbergen2@gmail.com>
2026-01-28 17:40:58 +0600

    Введены новые изменения.

--- lesson.py
+++ lesson.py
@@ -1,6 +1,27 @@
 class Mentor:
-    def __init__(self, name)
+    def __init__(self, name):
         self.name = name
-	self.junior = False
-    def info(self)
-	return 'hello', self.name
+        # self.junior = False
+    def info(self):
+        return 'hello', self.name
+
+class InfoAge(Mentor):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+    def info(self):
+        return f'{self.age} years old'
+
+class InfoExperience(Mentor):
+    def __init__(self, name, experience):
+        super().__init__(name)
+        self.experience = experience
+    def info(self):
+        return f'Опыт работы: {self.experience} лет '
+
+a = Mentor('Askat')
+print(a.info())
+b = InfoAge('Askan', 25)
+print(b.info())
+c = InfoExperience('Askan', 4)
+print(c.info())
