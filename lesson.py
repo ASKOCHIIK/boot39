377bd18e09f17df11eb902caf40735105813d29e
beksultanovcl <beksultanovcl7@gmail.com>
2026-01-30 10:47:47 +0600

    Изменение в коде

--- lesson.py
+++ lesson.py
@@ -1,6 +1,29 @@
 class Mentor:
-    def __init__(self, name)
+    def __init__(self, name, age):
         self.name = name
-	self.junior = False
-    def info(self)
-	return 'hello', self.name
+        self.senior = False
+        self.__age = age
+
+
+    def info(self):
+        return 'hello', self.name
+
+    def get_age(self):
+        return self.__age
+
+class Level(Mentor):
+    def lvl(self):
+        self.senior = True
+        return 'you are now senior mentor'
+
+a = Mentor('Askat', 25)
+print(a.get_age())
+
+
+
+
+
+
+
+
+
